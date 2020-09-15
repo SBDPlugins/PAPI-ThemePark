97022fb17c56b627de787d1d6c628b500584658b
stijnb1234 <stijnbannink23@gmail.com>
2020-09-15 20:22:38 +0200

    First commit!

---
+++ src/main/java/nl/sbdeveloper/themeparkexpansion/ThemeParkExpansion.java
@@ -0,0 +1,126 @@
+/*
+    ThemePark Expansion - Provides PlaceholderAPI placeholders for ThemePark
+    Copyright (C) 2020 SBDeveloper
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package nl.sbdeveloper.themeparkexpansion;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import nl.iobyte.themepark.api.API;
+import nl.iobyte.themepark.api.attraction.component.Status;
+import nl.iobyte.themepark.api.attraction.manager.StatusManager;
+import nl.iobyte.themepark.ridecount.RideCountAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+
+import java.util.concurrent.ExecutionException;
+
+public class ThemeParkExpansion extends PlaceholderExpansion {
+    @Override
+    public String getAuthor() {
+        return "sbdeveloper";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "tp";
+    }
+
+    @Override
+    public String getRequiredPlugin() {
+        return "ThemePark";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.4";
+    }
+
+    @Override
+    public boolean canRegister() {
+        return Bukkit.getPluginManager().isPluginEnabled(getRequiredPlugin());
+    }
+
+    private String color(String in) {
+        return ChatColor.translateAlternateColorCodes('&', in);
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String identifier){
+        if(identifier == null)
+            return "";
+
+        /* Non-player placeholders */
+
+        //%tp_status:ID%
+        if(identifier.startsWith("status")) {
+            String[] args = identifier.split(":");
+            if(args.length < 2)
+                return "";
+
+            String id = args[1];
+            if(!API.isAttraction(id))
+                return "";
+
+            Status status = API.getAttraction(id).getStatus();
+            return (StatusManager.getName(status));
+        }
+
+        //%tp_name:ID%
+        if(identifier.startsWith("name")) {
+            String[] args = identifier.split(":");
+            if(args.length < 2)
+                return "";
+
+            String id = args[1];
+            if(!API.isAttraction(id))
+                return "";
+
+            return color(API.getAttraction(id).getName());
+        }
+
+        if (player == null) {
+            return "";
+        }
+
+        /* Player placeholders */
+
+        //%tp_ridecount%
+        if (identifier.equals("ridecount")) {
+            int totalCount = API.getAttractions().values().stream().mapToInt(att -> {
+                try {
+                    return RideCountAPI.getCount(player.getUniqueId(), att).get();
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+                return 0;
+            }).sum();
+
+            return "" + totalCount;
+        } else if (identifier.startsWith("ridecount")) {
+            String[] args = identifier.split(":");
+            if(args.length < 2)
+                return "";
+
+            String id = args[1];
+            if(!API.isAttraction(id))
+                return "";
+
+            return "" + RideCountAPI.getCount(player.getUniqueId(), API.getAttraction(id));
+        }
+
+        return null;
+    }
+
+}
